9096a85aa2bb3303340b9aa06cd26c4f3a55da0b
JackVorobey23 <eygehavv27@gmail.com>
2021-10-20 21:35:22 +0300

    Lab1 done

---
+++ Task_2/Task_2.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Task_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter year: "); int year = Convert.ToInt32(Console.ReadLine());
+
+            int c = year / 100; int y = year - c * 100;
+            int day = 31;
+            while (((day + y + y / 4 + c / 4 - 2 * c + 20) % 7) != 0)
+                day--;
+
+            Console.WriteLine("Перехiд на зимовий час здiйсниться " + day + "го Жовтня!");
+        }
+    }
+}
