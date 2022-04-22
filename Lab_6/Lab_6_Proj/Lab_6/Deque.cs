97168b4b5c346f8470378502f42cd716a9e863b2
JackVorobey23 <64706161+JackVorobey23@users.noreply.github.com>
2022-04-22 14:40:41 +0300

    Add files via upload

---
+++ Lab_6/Lab_6_Proj/Lab_6/Deque.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_6
+{
+    internal class Deque
+    {
+        public Node? Tail;
+        public Node? Head;
+        public Deque()
+        {
+            this.Tail = null;
+            this.Head = null;
+        }
+        public bool insertHead(int data)
+        {
+            if (Head == null)
+            {
+                Head = new Node(data);
+                Head.Prev = Head.Next = Head;
+            }
+            else if (Tail == null)
+            {
+                Tail = Head;
+                Head = new Node(data);
+                Head.Prev = Head.Next = Tail;
+                Tail.Prev = Tail.Next = Head;
+            }
+            else
+            {
+                Node toAdd = new Node(data);
+                Head.Prev = toAdd;
+                Tail.Next = toAdd;
+                toAdd.Next = Head;
+                toAdd.Prev = Tail;
+                Head = toAdd;
+            }
+            return true;
+        }
+        public bool removeHead()
+        {
+            if (Head == null) return false;
+
+            if (Tail == null)
+            {
+                Head = null;
+                return false;
+            }
+
+            if (Head.Next == Tail)
+            {
+                Head = new Node(tail());
+                Head.Next = Head.Prev = Head;
+                Tail = null;
+            }
+            else
+            {
+                Head.Next.Prev = Tail;
+                Tail.Next = Head.Next;
+                Head = Head.Next;
+            }
+            return true;
+        }
+        public bool insertTail(int data)
+        {
+            if (Head == null)
+                insertHead(data);
+            else if (Tail == null)
+            {
+                Tail = new Node(data);
+                Tail.Prev = Tail.Next = Head;
+                Head.Next = Head.Prev = Tail;
+            }
+            else
+            {
+                Node toAdd = new Node(data);
+                Head.Prev = toAdd;
+                Tail.Next = toAdd;
+                toAdd.Next = Head;
+                toAdd.Prev = Tail;
+                Tail = toAdd;
+            }
+            return true;
+        }
+        public bool removeTail()
+        {
+            if (Head == null) return false;
+
+            if (Tail == null)
+                Head = null;
+
+            else if (Tail.Prev == Head)
+            {
+                Head = new Node(tail());
+                Head.Next = Head.Prev = Head;
+                Tail = null;
+            }
+            else
+            {
+                Tail.Prev.Next = Head;
+                Head.Prev = Tail.Prev;
+                Tail = Tail.Prev;
+            }
+            return true;
+        }
+        public int head()
+        {
+            return Head.data;
+        }
+        public int tail()
+        {
+            return Tail.data;
+        }
+        public bool isEmpty()
+        {
+            if(Head == null) return true;
+            return false;
+        }
+        public int size()
+        {
+            if(isEmpty()) return 0;
+
+            Node temp = Head;
+            int size = 1;
+            while (temp != Head.Prev)
+            {
+                temp = temp.Next;
+                size++;
+            }
+            return size;
+        }
+    }
+    internal class Node
+    {
+        public Node Prev { get; set; }
+        public Node Next { get; set; }
+        public int data;
+        public Node(int data)
+        {
+            this.data = data;
+        }
+    }
+}
